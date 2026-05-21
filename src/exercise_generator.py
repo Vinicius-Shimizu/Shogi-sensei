46ea1dca7538449a092fb233929d6cec25538905
Vinicius-Shimizu <viniciusshmz@usp.br>
2026-05-21 18:10:30 -0300

    Fixed YaneuraOu engine integration

--- src/exercise_generator.py
+++ src/exercise_generator.py
@@ -12,7 +12,7 @@ from pprint import pprint
 class ExerciseGenerator():
     def __init__(self, model: str, verbose=False, games_period = 7):
         self.verbose = verbose
-        # self.engine = self.start_yaneuraou_engine(model)
+        self.engine = self.start_yaneuraou_engine(model)
         self.exercises = None
         # self.games = []
         # for game in self.get_games(games_period):
@@ -27,22 +27,31 @@ class ExerciseGenerator():
             [model],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
             text=True,
-            bufsize=1
+            bufsize=1,
+            cwd="/app/model"
         )
 
         engine.stdin.write("usi\n")
         engine.stdin.flush()
         self.read_until(engine, "usiok")
+        print("Engine ok")
+        engine.stdin.write("setoption name EvalDir value /app/model/eval\n")
+        engine.stdin.flush()
+        engine.stdin.write("setoption name USI_Hash value 32\n")
+        engine.stdin.flush()
 
+        engine.stdin.write("setoption name Threads value 8\n")
+        engine.stdin.flush()
         engine.stdin.write("setoption name USI_OwnBook value false\n")
         engine.stdin.flush()
-
+        print("Book desconsidered")
         engine.stdin.write("isready\n")
         engine.stdin.flush()
+        print("isready sent!")
         self.read_until(engine, "readyok")
-
+        print("Engine ready!")
         return engine
 
 
@@ -53,7 +62,7 @@ class ExerciseGenerator():
                 raise RuntimeError("Engine stopped")
             line = line.strip()
 
-            if(self.verbose): print("[ENGINE]", line)
+            print("[ENGINE]", line)
 
             if keyword in line:
                 return line
@@ -74,7 +83,7 @@ class ExerciseGenerator():
                 print(f"Failed to access {url}: {e}")
                 continue
 
-            html = requests.get(url).text
+            html = response.text
             soup = BeautifulSoup(html, "html.parser")
             for a in soup.find_all("a"):
                 href = a.get("href")
@@ -122,13 +131,11 @@ class ExerciseGenerator():
 
 
 if __name__ == "__main__":
-    while True:
-        continue
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--model",
         type=str,
-        default="/app/model/yaneuraou"
+        default="/app/yaneuraou"
     )
     parser.add_argument("--v", action="store_true")
     args = parser.parse_args()
