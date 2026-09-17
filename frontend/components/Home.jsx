dcb9584fa44cc0cc798998eb0842e0e57c15bce5
Vinicius-Shimizu <viniciusshmz@usp.br>
2026-09-17 16:33:47 -0300

    feat: adjust home page

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -7,14 +7,14 @@ export default function Home(){
     const [started, setStarted] = useState(false);
     if(started) return <ExerciseList />;
 
-    return <div className="flex flex-col h-screen items-center p-4">
-        <div className="flex justify-center">
+    return <div className="grid grid-cols-3 h-screen p-4">
+        <div></div>
+        <div className="">
             <h1>Shogi-sensei</h1>
-            <LoginButton></LoginButton>
-        </div>
-        <div className="flex flex-col">
             <BeginButton onClick={() => {console.log("Starting list"); setStarted(true);}}></BeginButton>
-
+        </div>
+        <div className="justify-center">
+            <LoginButton></LoginButton>
         </div>
     </div>
 }
